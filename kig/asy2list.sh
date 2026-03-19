ba8934aa9ddd964dba9ad4680b368d331b4c2b9b
Maurizio Paolini <maurizio.paolini@unicatt.it>
2026-03-19 09:04:35 +0100

    Allow for more than one polygon

--- kig/asy2list.sh
+++ kig/asy2list.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 #
+# the resulting file contains a list of the vertices of the polygons
+#
+# the last line contain the number of polygons
+# each polygon is decribed in a block of lines:
+#   first line of block: number of vertices
+#   subsequent lines: coordinates of each vertex
+#
 
 function filter ()
 {
@@ -18,14 +25,33 @@ function filter ()
   done
 }
 
+function filter0 ()
+{
+  countcc=0
+  while read line
+  do
+    echo "$line" | filter >$tempfile
+    num=`cat $tempfile | wc -l`
+    echo "$num"
+    cat $tempfile
+    countcc=$[ $countcc + 1 ]
+  done
+  echo "$countcc"
+}
+
 tempfile="/tmp/asy2list_$$.tmp"
 
-cat | filter >$tempfile
+#
+# glue lines ending with '--' with the following
+#
+#cat | tr '\n' '#' | sed -e 's/--#/--/g' | sed -e 's/#/\n/g' | grep "^path polygon =" | head -n 1 | filter >$tempfile
+cat | tr '\n' '#' | sed -e 's/--#/--/g' | sed -e 's/#/\n/g' | grep "^path polygon =" | filter0 
 
-num=`cat $tempfile | wc -l`
 
-echo "$num"
-cat $tempfile
+#num=`cat $tempfile | wc -l`
+
+#echo "$num"
+#cat $tempfile
 
 rm $tempfile
 
